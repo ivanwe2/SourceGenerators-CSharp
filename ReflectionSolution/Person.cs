187d2f59dc302dfb8991d57d036102b2296770d7
Ivan Rusev <Ivan.Rusev@indeavr.com>
2024-10-09 16:48:59 +0300

    Add project files.

---
+++ ReflectionSolution/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionSolution
+{
+    internal class Person
+    {
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var propertyInfo in this.GetType().GetProperties())
+            {
+                var propertyName = propertyInfo.Name;
+                var propertyValue = propertyInfo.GetValue(this);
+
+                stringBuilder.Append($"{propertyName}:{propertyValue}; ");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ ReflectionSolution/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReflectionSolution;
+
+Console.WriteLine("Hello, World!");
+
+var person = new Person()
+{
+    FirstName = "Ivan",
+    MiddleName = "Rusev",
+    LastName = "Rusev"
+};
+
+Console.WriteLine(person);
